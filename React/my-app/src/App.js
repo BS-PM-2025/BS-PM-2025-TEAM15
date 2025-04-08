f3de1c253c08ebdc058086e6f3116c584068f08c
khmelfe@ac.sce.ac.il <khmelfe@ac.sce.ac.il>
2025-04-08 15:25:05 +0300

    React_fixes

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,14 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Wel from "./Pages/Wel";
+import Requestsubmissions_student from "./Pages/Requestsubmissions_student";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/sidebar";
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Wel" element={<Wel />} />
-      </Routes>
+    <div class="layout">
+      <div className="app-wrapper">
+        <div className="content-wrapper">
+          <Sidebar className="sidebar" />
+          <div className="main-content">
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/Wel" element={<Wel />} />
+              <Route path="/Requestsubmissions_student" element={<Requestsubmissions_student />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+      </div>
     </Router>
   );
 }
