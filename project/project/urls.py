5d46e681cd6dce45f224487076b29d21d865734e
yana <yanazl@ac.sce.ac.il>
2025-06-12 12:55:42 +0300

    kill admin

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from app.admin import safe_admin  # ✅ import your custom admin
+#from app.admin import safe_admin  # ✅ import your custom admin
 
 urlpatterns = [
-    path('admin/', safe_admin.urls),  # ✅ use your safe_admin
+    #path('admin/', safe_admin.urls),  # ✅ use your safe_admin
     path('', include('app.urls')),
 ]
