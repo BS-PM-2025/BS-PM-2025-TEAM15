3ee7f9206604991f2f12506d02e909c25d1fd2a5
yana <yanazl@ac.sce.ac.il>
2025-06-12 12:22:09 +0300

    commit for jenkins

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-# Register your models here.
+# Patch: Use a safe dummy admin site to prevent Jenkins crash
+class SafeAdminSite(AdminSite):
+    site_header = "Placeholder Admin"
+    site_title = "Placeholder Admin Portal"
+    index_title = "Welcome to Placeholder Admin"
+
+admin.site = SafeAdminSite()
